7ad1f135bc0cad4379c33a4425fa93156c48f329
Asiern <asnulorri@gmail.com>
2021-08-12 10:32:30 +0200

    added: BASS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-apt install -y gcc g++ gdb make cmake libwxgtk3.0-gtk3-dev libcurl4-openssl-dev
-mkdir build && cd build && cmake .. 
+set -e
+
+sudo apt install -y gcc g++ gdb make cmake libwxgtk3.0-gtk3-dev libcurl4-openssl-dev
+
+# Thirdparty BASS
+wget -P thirdparty/ http://uk.un4seen.com/files/bass24-linux.zip && cd thirdparty
+unzip bass24-linux.zip bass.h x64/libbass.so
+rm bass24-linux.zip
+
+cd .. && mkdir build && cd build && cmake .. 
