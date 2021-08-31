fecfd3297410493e608fbf82df0befbb16146138
Asiern <asnulorri@gmail.com>
2021-08-31 08:45:51 +0200

    added: BASS FX

--- scripts/FedoraSetup.sh
+++ scripts/FedoraSetup.sh
@@ -5,8 +5,15 @@ sudo dnf install -y gcc g++ gdb make cmake wxGTK-devel libcurl-devel
 
 
 # Thirdparty BASS
-wget -P thirdparty/ http://uk.un4seen.com/files/bass24-linux.zip && cd thirdparty
+wget -P thirdparty/BASS http://uk.un4seen.com/files/bass24-linux.zip && cd thirdparty/BASS
 unzip bass24-linux.zip bass.h x64/libbass.so
 rm bass24-linux.zip
+
+# BASS FX
+wget http://uk.un4seen.com/files/z/0/bass_fx24-linux.zip 
+unzip bass_fx24-linux.zip C/bass_fx.h x64/libbass_fx.so
+mv C/bass_fx.h ./bass_fx.h
+rm -rf C/
+rm bass_fx24-linux.zip
 
 cd .. && mkdir build && cd build && cmake .. 
