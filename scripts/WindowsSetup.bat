d05cabed6126e38e41ecc8fafd9f5af85ec6ef32
Asiern <asnulorri@gmail.com>
2021-08-31 11:23:59 +0200

    added: BASS FX

--- scripts/WindowsSetup.bat
+++ scripts/WindowsSetup.bat
@@ -9,8 +9,12 @@ set WorkDir=%cd%
 Rem Download BASS
 bitsadmin /transfer BASS_Download /download /priority normal http://uk.un4seen.com/files/bass24.zip %WorkDir%\thirdparty\bass.zip
 
+Rem Download BASS FX
+bitsadmin /transfer BASS_FX_Download /download /priority normal http://uk.un4seen.com/files/bass_fx24.zip %WorkDir%\thirdparty\bass_fx.zip
+
 Rem Unzip files
 powershell Expand-Archive %WorkDir%\thirdparty\bass.zip -DestinationPath %WorkDir%\thirdparty\BASS
+powershell Expand-Archive %WorkDir%\thirdparty\bass_fx.zip -DestinationPath %WorkDir%\thirdparty\BASS_FX
 
 Rem Cleanup
 DEL  %WorkDir%\thirdparty\*.zip
