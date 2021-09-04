a7ad0608964693cbb93b32dfdfdd1a489cb133db
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2021-09-04 22:04:44 +0200

    Update UbuntuSetup.sh

--- scripts/UbuntuSetup.sh
+++ scripts/UbuntuSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#sudo apt install -y gcc g++ gdb make cmake libwxgtk3.0-gtk3-dev libcurl4-openssl-dev
+sudo apt install -y gcc g++ gdb make cmake libwxgtk3.0-gtk3-dev libcurl4-openssl-dev
 
 # Thirdparty BASS
 wget -P thirdparty/BASS/ http://uk.un4seen.com/files/bass24-linux.zip && cd thirdparty/BASS
