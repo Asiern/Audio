77e9469dff364e7643a9bac34a438ed60c8b4a65
Asiern <asnulorri@gmail.com>
2021-09-07 12:44:55 +0200

    added: bass enc

--- scripts/FedoraSetup.sh
+++ scripts/FedoraSetup.sh
@@ -7,13 +7,21 @@ sudo dnf install -y gcc g++ gdb make cmake wxGTK-devel libcurl-devel
 # Thirdparty BASS
 wget -P thirdparty/BASS http://uk.un4seen.com/files/bass24-linux.zip && cd thirdparty/BASS
 unzip bass24-linux.zip bass.h x64/libbass.so
-rm bass24-linux.zip
 
 # BASS FX
 wget http://uk.un4seen.com/files/z/0/bass_fx24-linux.zip 
 unzip bass_fx24-linux.zip C/bass_fx.h x64/libbass_fx.so
 mv C/bass_fx.h ./bass_fx.h
 rm -rf C/
-rm bass_fx24-linux.zip
+
+# BASS ENC
+wget https://www.un4seen.com/files/bassenc24-linux.zip
+unzip bassenc24-linux.zip bassenc.h x64/libbassenc.so
+
+# BASS ENC MP3
+wget https://www.un4seen.com/files/bassenc_mp324-linux.zip 
+unzip bassenc_mp324-linux.zip bassenc_mp3.h x64/libbassenc_mp3.so
+
+rm *.zip
 
 cd ../.. && mkdir build && cd build && cmake .. 
